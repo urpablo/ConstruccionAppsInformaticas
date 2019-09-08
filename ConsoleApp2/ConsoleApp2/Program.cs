057234bde8f9db0e77eb451d1efc13411b18d06b
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-08 11:00:48 -0300

    nuevo cambio

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("aloo");
+            Console.WriteLine("com va todo");
         }
     }
 }
