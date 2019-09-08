ff4756cca70813d24a73fe1d59e2b73a3d94ad67
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-08 18:03:01 -0300

    .

---
+++ Ejercicio.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese su texto");
+            Console.Write("su texto es: "+ Console.ReadLine());
+            Console.ReadKey();
+
+        }
+    }
+}
