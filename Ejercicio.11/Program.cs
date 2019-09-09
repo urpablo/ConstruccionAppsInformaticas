d79f867a6c1d249815861c97b0eec374fff15692
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-09 20:39:05 -0300

    .

---
+++ Ejercicio.11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring;
+
+            Console.WriteLine("Ingrese su texto");
+          
+            mystring = Console.ReadLine();
+            mystring = mystring.Replace("á", "a");
+            mystring = mystring.Replace("é", "e");
+            mystring = mystring.Replace("í", "i");
+            mystring = mystring.Replace("ó", "o");
+            mystring = mystring.Replace("ú", "u");
+
+            Console.WriteLine(mystring);
+            Console.ReadKey();
+
+        }
+    }
+}
