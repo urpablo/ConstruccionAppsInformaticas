86ecd02f2d6d78dd25f1980e91b3b3b8ba6a0b91
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-09 20:21:02 -0300

    .

---
+++ Ejercicio.9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingreso;
+            Console.WriteLine("Ingrese un texto");
+            ingreso = Console.ReadLine();
+            Console.WriteLine("Su ingreso en mayusculas es: "+ingreso.ToUpper());
+            Console.ReadKey();
+
+        }
+    }
+}
