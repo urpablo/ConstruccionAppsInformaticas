20ea2af3bd742d92955ce06ba395ee466ed28b65
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-09 20:13:04 -0300

    .

---
+++ Ejercicio.8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1; //= "books pencil pen ruler";
+            string s2 = "fin";
+
+            Console.WriteLine("Ingrese una frase");
+            s1 = Console.ReadLine();
+            if (s1.Contains(s2) && s2 != "")
+            {
+                Console.WriteLine("La palabra fin se encuentra en el texto");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("La palabra fin NO se encuentra en el texto");
+                Console.ReadLine();
+            }
+        }
+    }
+}
