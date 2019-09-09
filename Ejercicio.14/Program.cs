f6aefd713dba0089e82e7414bdb3d27879fe6ccd
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-09 20:55:36 -0300

    .

---
+++ Ejercicio.14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto1;
+            string texto2;
+
+            Console.WriteLine("Ingrese el texto 1 ");
+            texto1 = Console.ReadLine();
+            Console.WriteLine("Ingrese el texto 2 ");
+            texto2 = Console.ReadLine();
+
+            if (texto1.ToUpper() == texto2.ToUpper())
+            {
+                Console.WriteLine("Las lineas son iguales");
+            }
+            else
+            {
+                Console.WriteLine("las lineas no son iguales");
+            }
+            Console.ReadKey();
+        }
+    }
+}
