0ab7a9aa3e9223f70d5eea1b22bce900738e9a8f
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-10 20:04:03 -0300

    .

---
+++ 15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingreso;
+            bool flag1 = false;
+            Int32 nroint;
+            bool flag2 = false;
+            decimal nrodec;
+
+            Console.WriteLine("Ingrese un texto a comprobar");
+            ingreso = Console.ReadLine();
+
+            flag1 = Int32.TryParse(ingreso, out nroint);
+            flag2 = decimal.TryParse(ingreso, out nrodec);
+
+            if (flag1 == true || flag2 == true)
+            {
+                Console.WriteLine("el valor introducido es numerico");
+            }
+            else
+            {
+                Console.WriteLine("el valor introducido NO es numerico");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
