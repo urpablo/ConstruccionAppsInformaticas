b209e63ec8c7cdd8227b209437b1f526bd110410
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-14 21:29:46 -0300

    .

---
+++ Ejercicio.21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fechaactual = DateTime.Now;
+            Console.Write("la fecha actual es: " + fechaactual);
+            Console.ReadKey();
+        }
+    }
+}
