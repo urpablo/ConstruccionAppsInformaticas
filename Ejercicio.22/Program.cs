d8c298f9001ba6cf3df21d34d1e8cb6e82e1d632
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-14 21:38:24 -0300

    .

---
+++ Ejercicio.22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fecha = DateTime.Now;
+            string strfecha;
+
+            strfecha = fecha.ToString("dd/MM/yyyy");
+
+            Console.WriteLine(strfecha);
+            Console.ReadKey();
+        }
+    }
+}
