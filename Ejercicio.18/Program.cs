d53b536ea30cc6f3b8caad4026db8dfbe92f1c2e
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-14 21:22:49 -0300

    .

---
+++ Ejercicio.18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strnro1;
+            Int32 decimales = 2;
+            decimal nro1;
+            decimal redondeado;
+
+            Console.WriteLine("ingrese numero decimal: ");
+            strnro1= Console.ReadLine();
+
+            nro1 = decimal.Parse(strnro1);
+            redondeado=Decimal.Round(nro1, decimales);
+
+
+            Console.Write(redondeado);
+            Console.ReadKey();
+
+
+        }
+    }
+}
