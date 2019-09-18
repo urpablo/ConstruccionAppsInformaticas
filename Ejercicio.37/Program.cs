4a48c2db6b7e1d83398ba63fc06e40149ae50912
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-18 19:46:26 -0300

    .

---
+++ Ejercicio.37/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena;
+            bool flag = false;
+
+            do
+            {
+                Console.WriteLine("Ingrese un texto entre 10 y 20 caracteres");
+                cadena = Console.ReadLine();
+
+                if (cadena.Length > 20 || cadena.Length < 10)
+                {
+                    Console.WriteLine("el texto ingresado no cumple con las condiciones");
+                    flag = false;
+                }
+                else
+                {
+                    flag = true;
+                }
+            } while (flag==false);
+
+            Console.WriteLine(cadena);
+            Console.ReadKey();
+
+        }
+    }
+}
