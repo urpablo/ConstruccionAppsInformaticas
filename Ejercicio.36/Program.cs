f099ae68845cad8d1d43372d25c2144a21aab680
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-18 19:37:44 -0300

    .

---
+++ Ejercicio.36/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strfecha;
+            bool flag = false;
+            DateTime fecha;
+
+            do
+            {
+                Console.WriteLine("Ingrese una fecha");
+                strfecha = Console.ReadLine();
+
+                flag = DateTime.TryParse(strfecha, out fecha);
+
+                if (flag == false)
+                {
+                    Console.WriteLine("Ingrese una fecha valida");
+                }
+
+            } while (flag==false);
+
+            Console.WriteLine(fecha);
+            Console.ReadKey();
+
+
+        }
+    }
+}
