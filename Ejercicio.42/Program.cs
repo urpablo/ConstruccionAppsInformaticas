92becfb5d75c92db7b25aa1bd8a363b7f90f2665
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-29 20:28:53 -0300

    .

---
+++ Ejercicio.42/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una palabra");
+            String str = Console.ReadLine();
+           
+
+            // copy the string str to chars  
+            // character array & it will start 
+            // copy from 'G' to 's', i.e.  
+            // beginning to ending of string 
+            char[] chars = str.ToCharArray();
+
+            Console.WriteLine("String: " + str);
+            Console.WriteLine("Character array :");
+
+            // to display the resulted character array 
+            //for ( i = 0; i < chars.Length; i++)
+            //   Console.Write(" " + chars[i]);
+
+            Array.Reverse(chars);
+            foreach (char i in chars)
+                Console.WriteLine(i);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio.43/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strNumero;
+            decimal Numero;
+            bool flag = false;
+            var ListaNumeros = new List<decimal>();
+            bool flagSalida = false;
+            decimal suma = 0;
+            decimal max = 0;
+            decimal min = 0;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese un numero o -1 para salir");
+                    strNumero = Console.ReadLine();
+                    flag = decimal.TryParse(strNumero, out Numero);
+
+                    if (flag == false)
+                    {
+                        Console.WriteLine("Debe ingresar un numero");
+                    }
+
+                } while (flag == false);
+                
+                if (Numero == -1&&ListaNumeros.Count()==0)
+                {
+                    return;
+                }
+                else if(Numero==-1)
+                {
+                    flagSalida = true;
+                }
+                else
+                {
+                    ListaNumeros.Add(Numero);
+                }
+
+                
+                foreach (decimal s in ListaNumeros)
+                {
+                    
+                    if (s > max)
+                    {
+                        max = s;
+                    }
+                }
+
+                min = ListaNumeros[0];
+                foreach (decimal s in ListaNumeros)
+                {
+
+                    if (s < min)
+                    {
+                        min = s;
+                    }
+                }
+
+            } while (flagSalida == false);
+
+            foreach (decimal s in ListaNumeros)
+                suma += s;
+
+
+
+            //ListaNumeros.Count();
+
+            Console.WriteLine("suma: "+suma);
+            Console.WriteLine("promedio: " + (suma/ListaNumeros.Count()));
+            Console.WriteLine("maximo: "+max);
+            Console.WriteLine("minimo: " + min);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
