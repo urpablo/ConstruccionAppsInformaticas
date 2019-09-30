56861895e1d1bdd0977b2df3feefbe607aa54ac7
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-09-30 00:04:02 -0300

    .

---
+++ Ejercicio.44/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comando;
+            bool flag = false;
+            var ListaNombres = new List<string>();
+
+            do
+            {
+                Console.WriteLine("Ingrese un comando");
+                comando = Console.ReadLine();
+                if ()
+                {
+
+                }
+                else if ()
+                {
+
+                }
+                else if ()
+                {
+
+                }
+                else if ()
+                {
+
+                }
+                else
+                {
+
+                }
+
+            } while (flag == false);
+        }
+    }
+}
