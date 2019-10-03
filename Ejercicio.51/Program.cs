069157c9e1c69fc979b025da42f6d3a31aa381d6
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-10-03 10:54:04 -0300

    .

---
+++ Ejercicio.51/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Ejercicio._51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string linea;
+            // Set a variable to the Documents path.
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            // Append text to an existing file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt"), true))
+            {
+                do
+                {
+                    Console.WriteLine("ingrese una cadena");
+                    linea = Console.ReadLine();
+                    outputFile.WriteLine(linea);
+                } while (linea != "fin");
+                
+            }
+        }
+    }
+}
