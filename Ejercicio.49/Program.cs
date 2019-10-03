e19c87247cbd17504f6b0aa1743e70e73ffa34ac
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-10-03 10:32:37 -0300

    .

---
+++ Ejercicio.49/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ejercicio._49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Pablo\Desktop\MyTest.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hola Mundo");
+                    //sw.WriteLine("And");
+                    //sw.WriteLine("Welcome");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ya existe el archivo ameo");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ Ejercicio.50/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Ejercicio._50
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            FileDialog fbd = new OpenFileDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                Console.WriteLine(fbd.FileName);
+                
+
+            }
+
+            string text = System.IO.File.ReadAllText(fbd.FileName);
+            Console.WriteLine(text);
+            Console.ReadKey();
+
+
+        }
+    }
+}
